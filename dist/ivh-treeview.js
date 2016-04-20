74818b78bdf6a354fde527c569641f4709e5f99c
569485 <Emo_Jacob@bah.com>
2016-04-20 08:33:41 -0400

    reverted indeterminate change

--- dist/ivh-treeview.js
+++ dist/ivh-treeview.js
@@ -886,7 +886,7 @@ angular.module('ivh.treeview')
         node[selectedAttr] = true;
         node[indeterminateAttr] = false;
       } else {
-        node[selectedAttr] = true;
+        node[selectedAttr] = false;
         node[indeterminateAttr] = true;
       }
     };
